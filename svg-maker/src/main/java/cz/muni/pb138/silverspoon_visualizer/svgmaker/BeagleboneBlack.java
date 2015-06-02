9fc3926f92888f00ab75e2755d24aceff6647d4b
Martin Zilak <tinnyx@gmail.com>
2015-06-02 23:46:46 +0200

    completed route ending in ethernet for BeagleboneBlack

--- svg-maker/src/main/java/cz/muni/pb138/silverspoon_visualizer/svgmaker/BeagleboneBlack.java
+++ svg-maker/src/main/java/cz/muni/pb138/silverspoon_visualizer/svgmaker/BeagleboneBlack.java
@@ -3,6 +3,7 @@ package cz.muni.pb138.silverspoon_visualizer.svgmaker;
 import cz.muni.pb138.silverspoon_visualizer.parser.GpioPathObject;
 import cz.muni.pb138.silverspoon_visualizer.parser.PathObject;
 import cz.muni.pb138.silverspoon_visualizer.parser.Route;
+import cz.muni.pb138.silverspoon_visualizer.parser.SuccessionTypes;
 import org.w3c.dom.Element;
 
 import java.util.regex.Matcher;
@@ -31,21 +32,41 @@ public class BeagleboneBlack extends Board {
         RouteDrawer routeDrawer = new RouteDrawer(route, svgDocument);
         routeDrawer.drawRoute();
 
+        double boardTranslate = 0;
+
         //next part handles expanding the .svg
         double routeMainRectWidth = Double.parseDouble(svgDocument.getElementById("main_rect").getAttribute("width"));
         if (routeMainRectWidth > 275) {
+            boardTranslate = routeMainRectWidth - 275;
+            
             Element svgBodyRect = svgDocument.getElementById("body");
-            svgBodyRect.setAttribute("width", String.valueOf((Double.parseDouble(svgBodyRect.getAttribute("width")) + routeMainRectWidth - 275)));
+            svgBodyRect.setAttribute("width", String.valueOf((Double.parseDouble(svgBodyRect.getAttribute("width")) + boardTranslate)));
 
             Element svgMovableGroup = svgDocument.getElementById("movable");
-            svgMovableGroup.setAttribute("transform", "translate(" + (routeMainRectWidth - 275) + " 0)");
+            svgMovableGroup.setAttribute("transform", "translate(" + boardTranslate + " 0)");
 
             Element svgMainElement = svgDocument.getDocumentElement();
-            svgMainElement.setAttribute("width", String.valueOf((Double.parseDouble(svgMainElement.getAttribute("width")) + routeMainRectWidth - 275)));
+            svgMainElement.setAttribute("width", String.valueOf((Double.parseDouble(svgMainElement.getAttribute("width")) + boardTranslate)));
         }
 
         //next part handles drawing dotted line from starting pin to first route module
         Element svgRouteElement = svgDocument.getElementById("dotted_lines");
+
+        double translateX = 0;
+        double translateY = 0;
+
+        Element svgRouteGroup = svgDocument.getElementById("route");
+        String routeTransform = svgRouteGroup.getAttribute("transform");
+
+        Pattern pattern = Pattern.compile("translate\\((\\d*)\\s(\\d*)\\)");
+        Matcher matcher = pattern.matcher(routeTransform);
+
+        while (matcher.find()) {
+            translateX = Double.parseDouble(matcher.group(1));
+            translateY = Double.parseDouble(matcher.group(2));
+        }
+
+        boolean routeFromStartAbove = true;
 
         PathObject pathObject = route.getFirst();
         if (pathObject instanceof GpioPathObject) {
@@ -68,34 +89,59 @@ public class BeagleboneBlack extends Board {
                 }
             }
 
-            Element svgRouteGroup = svgDocument.getElementById("route");
-            String routeTransform = svgRouteGroup.getAttribute("transform");
-
-            Pattern pattern = Pattern.compile("translate\\((\\d*)\\s(\\d*)\\)");
-            Matcher matcher = pattern.matcher(routeTransform);
-
-            double translateX = 0;
-            double translateY = 0;
-
-            while (matcher.find()) {
-                translateX = Double.parseDouble(matcher.group(1));
-                translateY = Double.parseDouble(matcher.group(2));
-            }
-
-            Element module1 = svgDocument.getElementById("module_1_rect");
+            Element firstModule = svgDocument.getElementById("module_1_rect");
 
             double currPinX = Double.parseDouble(currPin.getAttribute("x")) + 7;
             double currPinY = Double.parseDouble(currPin.getAttribute("y")) + 7;
-            double module1x = Double.parseDouble(module1.getAttribute("x")) + translateX;
-            double module1y = Double.parseDouble(module1.getAttribute("y")) + translateY;
-            double module1height = Double.parseDouble(module1.getAttribute("height"));
-            double module1middleY = module1y + (module1height/2);
+            double firstModuleX = Double.parseDouble(firstModule.getAttribute("x")) + translateX;
+            double firstModuleY = Double.parseDouble(firstModule.getAttribute("y")) + translateY;
+            double firstModuleHeight = Double.parseDouble(firstModule.getAttribute("height"));
+            double firstModuleMiddleY = firstModuleY + (firstModuleHeight/2);
+
+            if (currPinY > firstModuleMiddleY) {
+                routeFromStartAbove = false;
+            }
 
             int offset = 35;
-            routeDrawer.drawDottedLine(svgRouteElement, currPinX, currPinY, currPinX, (currPinY + module1middleY)/2, pinBackup);
-            routeDrawer.drawDottedLine(svgRouteElement, currPinX, (currPinY + module1middleY)/2, module1x - offset, (currPinY + module1middleY)/2);
-            routeDrawer.drawDottedLine(svgRouteElement, module1x - offset, (currPinY + module1middleY)/2, module1x - offset, module1middleY);
-            routeDrawer.drawDottedLine(svgRouteElement, module1x - offset, module1middleY, module1x, module1middleY);
+            routeDrawer.drawDottedLine(svgRouteElement, currPinX, currPinY, currPinX, (currPinY + firstModuleMiddleY)/2, pinBackup);
+            routeDrawer.drawDottedLine(svgRouteElement, currPinX, (currPinY + firstModuleMiddleY)/2, firstModuleX - offset, (currPinY + firstModuleMiddleY)/2, false);
+            routeDrawer.drawDottedLine(svgRouteElement, firstModuleX - offset, (currPinY + firstModuleMiddleY)/2, firstModuleX - offset, firstModuleMiddleY, false);
+            routeDrawer.drawDottedLine(svgRouteElement, firstModuleX - offset, firstModuleMiddleY, firstModuleX, firstModuleMiddleY, true);
+        }
+
+        //next part handles drawing dotted line from last route module to route end
+        while (pathObject.getNextType() == SuccessionTypes.STRAIGHT) {
+            pathObject = pathObject.getNext().get(0);
+        }
+
+        if (pathObject.getNextType() == SuccessionTypes.ETHERNET) {
+            Element ethernet = svgDocument.getElementById("ethernet");
+
+            double ethernetX = Double.parseDouble(ethernet.getAttribute("x"));
+            double ethernetY = Double.parseDouble(ethernet.getAttribute("y"));
+            double ethernetHeight = Double.parseDouble(ethernet.getAttribute("height"));
+            double ethernetWidth = Double.parseDouble(ethernet.getAttribute("width"));
+            double ethernetTwoThirdsX = ethernetX + (ethernetWidth * 0.66);
+
+            int moduleCount = route.getLength();
+            Element lastModule = svgDocument.getElementById("module_" + String.valueOf(moduleCount) + "_rect");
+
+            double lastModuleX = Double.parseDouble(lastModule.getAttribute("x")) + Double.parseDouble(lastModule.getAttribute("width")) + translateX;
+            double lastModuleY = Double.parseDouble(lastModule.getAttribute("y")) + translateY;
+            double lastModuleHeight = Double.parseDouble(lastModule.getAttribute("height"));
+            double lastModuleMiddleY = lastModuleY + (lastModuleHeight/2);
+
+            int spacer = 35;
+            routeDrawer.drawDottedLine(svgRouteElement, lastModuleX, lastModuleMiddleY, lastModuleX + spacer, lastModuleMiddleY, false);
+            if (routeFromStartAbove) {
+                routeDrawer.drawDottedLine(svgRouteElement, lastModuleX + spacer ,lastModuleMiddleY, lastModuleX + spacer, ethernetY + ethernetHeight + spacer, false);
+                routeDrawer.drawDottedLine(svgRouteElement, lastModuleX + spacer, ethernetY + ethernetHeight + spacer, ethernetTwoThirdsX, ethernetY + ethernetHeight + spacer, false);
+                routeDrawer.drawDottedLine(svgRouteElement, ethernetTwoThirdsX, ethernetY + ethernetHeight + spacer, ethernetTwoThirdsX, ethernetY + ethernetHeight, true);
+            } else {
+                routeDrawer.drawDottedLine(svgRouteElement, lastModuleX + spacer ,lastModuleMiddleY, lastModuleX + spacer, ethernetY - spacer, false);
+                routeDrawer.drawDottedLine(svgRouteElement, lastModuleX + spacer, ethernetY - spacer, ethernetTwoThirdsX, ethernetY - spacer, false);
+                routeDrawer.drawDottedLine(svgRouteElement, ethernetTwoThirdsX, ethernetY - spacer, ethernetTwoThirdsX, ethernetY, true);
+            }
         }
     }
 }
